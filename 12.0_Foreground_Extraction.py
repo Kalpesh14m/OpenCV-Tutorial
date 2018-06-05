cf967202569ed31c24e5f66b8c577c634c30decb
Kalpesh Mali <kalpesh.1.mali@gmail.com>
2018-06-05 12:27:33 +0530

    Updated
    In above code try to use your image and set co-ordinates of your images in "rect = (x, y, w, h)".

--- 12.0_Foreground_Extraction.py
+++ 12.0_Foreground_Extraction.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('Images/Foreground-Extraction.jpg')
+img = cv2.imread('Images/Foreground_Extraction.png')
 mask = np.zeros(img.shape[:2],np.uint8)
 
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
 
-rect = (161,79,150,150)
+rect = (100,100,200,300)
 
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
